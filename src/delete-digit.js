47db899e55c6267e2d3d01b4b0ba8ab7f075fa91
Sergeeva Anastasia <Anastasiapv@bk.ru>
2022-09-14 13:14:20 +0300

    feat: Added solutions to problems

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,9 +11,11 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+  
+  let str = n.toString();
+  num = str.slice(0,  str.length - 1);
+ return num;
 }
 
 module.exports = {
--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,15 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+
+function createDreamTeam(arr) {
+
+ let arr = ['Matt', 'Ann', 'Dmitry', 'Max'];
+ let newArr = [];
+ 
+ Array.from(arr[0])[0]
+  return newArr;
 }
 
 module.exports = {
